6188a19ec28e9011be5fec4a117d6910e4fa917f
vals <hei.teddy.3@gmail.com>
2026-08-13 23:25:29 +0300

    fix: rename user table to app_user

--- src/main/java/api/poja/app/model/User.java
+++ src/main/java/api/poja/app/model/User.java
@@ -6,6 +6,7 @@ import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,6 +15,7 @@ import lombok.ToString;
 import org.hibernate.annotations.GenericGenerator;
 
 @Entity
+@Table(name = "app_user")
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
